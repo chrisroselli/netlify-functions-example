e23dde2fb2d02c96891aca6338539cc4e69b261e
chrisroselli <croselli.basementsystems@gmail.com>
2019-08-28 13:02:12 -0400

    updatrs

--- src/lambda/cleancss.js
+++ src/lambda/cleancss.js
@@ -1,9 +1,6 @@
 const CleanCSS = require('clean-css');
-const fs = require('fs');
-
 
 exports.handler = function(event, context, callback) {
-     var options = { /* options */ };
      var source = `
 @import url(https://a80427d48f9b9f165d8d-c913073b3759fb31d6b728a919676eab.ssl.cf1.rackcdn.com/v3/colorbox/colorbox.css);
 @import url(https://cdn.jsdelivr.net/jquery.slick/1.5.9/slick.css);`;
--- src/lambda/purgecss.js
+++ src/lambda/purgecss.js
@@ -1,14 +1,21 @@
 import Purgecss from 'purgecss'
+import purgeHtml from 'purge-from-html'
 
 exports.handler = async function(event) {
   const purgeCss = new Purgecss({
     content: ['test.html'],
-    css: ['test.css']
+    css: ['test.css'],
+    extractors: [
+      {
+        extractor: purgeHtml,
+        extensions: ['html']
+      }
+    ]
   })
-  const purgecssResult = purgeCss.purge()
- 
+  const purgecssResult = JSON.stringify(purgeCss.purge());
+  let cleanString = purgecssResult.substring(1,purgecssResult.length-1).replace(/[\\]/g, "");
   return {
     statusCode: 200,
-    body: JSON.stringify(purgecssResult)
+    body: cleanString
   };
 };
