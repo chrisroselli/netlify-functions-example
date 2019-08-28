bbb231a320c77851c78013992e8121f5edd5a5fa
chrisroselli <croselli.basementsystems@gmail.com>
2019-08-28 15:22:21 -0400

    removed extractor

--- src/lambda/purgecss.js
+++ src/lambda/purgecss.js
@@ -1,16 +1,9 @@
 import Purgecss from 'purgecss'
-import purgeHtml from 'purge-from-html'
 
 exports.handler = async function(event) {
   const purgeCss = new Purgecss({
     content: ['test.html'],
-    css: ['test.css'],
-    extractors: [
-      {
-        extractor: purgeHtml,
-        extensions: ['html']
-      }
-    ]
+    css: ['test.css']
   })
   const purgecssResult = JSON.stringify(purgeCss.purge());
   let cleanString = purgecssResult.substring(1,purgecssResult.length-1).replace(/[\\]/g, "");
